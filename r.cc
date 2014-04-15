526df7a4e5d800fa3c025d275b85d84528c961fc
Peter Szabo <pts@fazekas.hu>
2014-04-15 23:49:44 +0200

    added sout << ... stream output and formatters for integral and floating point types

--- r.cc
+++ r.cc
@@ -12,6 +12,9 @@
 
 #include <string>
 
+// For eGlibc, math.h defines this, for uClibc, it doesn't.
+// #define NAN (__builtin_nanf (""))
+
 void die(const char *msg) {
   fprintf(stderr, "fatal: %s\n", msg);
   exit(1);
@@ -25,8 +28,21 @@ static inline bool is_digit(int c) {
   return c - '0' + 0U <= 9;
 }
 
+template<class T>static inline T *notnull(T *t) {
+  if (!t) die("NULL pointer found.");
+  return t;
+}
+
+template<class T>static inline const T *notnull(const T *t) {
+  if (!t) die("NULL pointer found.");
+  return t;
+}
+
+#define assume_notnull(t) (t)
+
 class Status {
  public:
+  // TODO(pts): Don't inline these constructs and destrutors?
   Status(bool is_ok)
       : msg_(is_ok ? NULL : "Something failed."), is_used_(false) {}
   // msg is owned externally.
@@ -125,24 +141,24 @@ DecReader read_dec(FILE *f) { return DecReader(f); }
 
 class DecOutI8 {
  public:
-  DecOutI8(int8_t *p): p_(p) {}  // TODO(pts): CHECK_NOTNULL(p).
+  DecOutI8(int8_t *p): p_(notnull(p)) {}
   int8_t *get() const { return p_; }
  private:
   int8_t *p_;
 };
 class DecOutI16 {
  public:
-  DecOutI16(int16_t *p): p_(p) {}  // TODO(pts): CHECK_NOTNULL(p).
+  DecOutI16(int16_t *p): p_(notnull(p)) {}
   int16_t *get() const { return p_; }
  private:
   int16_t *p_;
 };
 
-FILE *operator>>(FILE *f, const DecOutI8 &out) {
+static inline FILE *operator>>(FILE *f, const DecOutI8 &out) {
   read_dec(f, out.get());
   return f;
 }
-FILE *operator>>(FILE *f, const DecOutI16 &out) {
+static inline FILE *operator>>(FILE *f, const DecOutI16 &out) {
   read_dec(f, out.get());
   return f;
 }
@@ -150,7 +166,7 @@ FILE *operator>>(FILE *f, const DecOutI16 &out) {
 DecOutI8  dec(int8_t  *p) { return p; }
 DecOutI16 dec(int16_t *p) { return p; }
 
-Status read_literal(FILE *f, const char *msg, intptr_t size) {
+Status read_literal(FILE *f, const char *msg, uintptr_t size) {
   for (; size > 0; ++msg, --size) {
     int c = getc(f);
     // TODO(pts): Report what literal (msg0).
@@ -165,21 +181,24 @@ Status read_literal(FILE *f, const char *msg, intptr_t size) {
 
 class LiteralOut {
  public:
-  LiteralOut(const char *msg, intptr_t size): msg_(msg), size_(size) {}
+  LiteralOut(const char *msg, uintptr_t size): msg_(msg), size_(size) {}
   LiteralOut(const char *msg): msg_(msg), size_(strlen(msg)) {}
   Status read(FILE *f) const { return read_literal(f, msg_, size_); }
  private:
   const char * const msg_;  // Owned externally.
-  const intptr_t size_;
+  const uintptr_t size_;
 };
 
 // TODO(pts): Move literal, dec etc. to a namespace.
 LiteralOut literal(const char *msg) { return msg; }
-LiteralOut literal(const char *msg, intptr_t size) {
+LiteralOut literal(const char *msg, uintptr_t size) {
   return LiteralOut(msg, size);
 }
 
-FILE *operator>>(FILE *f, const LiteralOut &out) { out.read(f); return f; }
+static inline FILE *operator>>(FILE *f, const LiteralOut &out) {
+  out.read(f);
+  return f;
+}
 
 // Check that next character is not a whitespace.
 Status peek_nows(FILE *f) {
@@ -193,15 +212,185 @@ Status peek_nows(FILE *f) {
 }
 
 class NowsOut {};
-NowsOut nows;
+extern NowsOut nows;
 
-FILE *operator>>(FILE *f, NowsOut) { peek_nows(f); return f; }
+static inline FILE *operator>>(FILE *f, NowsOut) { peek_nows(f); return f; }
 
+// --- Float formatting.
+
+// TODO(pts): Also for long double.
+void fmt_double(double d, char outbuf[32]) {
+  // Assuming IEEE 754 64-bit double.
+  struct AssertDoubleSizeIs8 { int DoubleSizeIs8 : sizeof(d) == 8; };
+  char *p;
+  int c;
+  sprintf(outbuf, "%.17g", d);
+  for (c = 0, p = outbuf; *p && *p != 'e'; ++p) {
+    c += *p - '0' + 0U <= 9U;
+  }
+  /* Used all digits, e.g. "1.2300000000000001e+202", "0.3333333333333333" */
+  if (c >= 17) {  /* Can be 17 or 18. */
+    char tmp[32];
+    double d16;
+    sprintf(tmp, "%.16g", d);
+    if (1 == sscanf(tmp, "%lf", &d16) && d == d16) {
+      strcpy(outbuf, tmp);
+    }
+  }
+}
+
+void fmt_float(float d, char outbuf[24]) {
+  // Assuming IEEE 754 32-bit float.
+  struct AssertFloatSizeIs4 { int FloatSizeIs4 : sizeof(d) == 4; };
+  char *p;
+  int c;
+  sprintf(outbuf, "%.9g", d);
+  for (c = 0, p = outbuf; *p && *p != 'e'; ++p) {
+    c += *p - '0' + 0U <= 9U;
+  }
+  if (c >= 9) {
+    char tmp[24];
+    float d8;
+    sprintf(tmp, "%.8g", d);
+    if (1 == sscanf(tmp, "%f", &d8) && d == d8) {
+      strcpy(outbuf, tmp);
+    }
+  }
+}
+
+
+// --- Output.
+
+class Io {};
+extern Io io;
+
+class FileObj {
+ public:
+  FileObj(FILE *f): f_(assume_notnull(f)) {}
+  Status flush() {
+    return fflush(f_) == 0;
+  }
+  Status write(const void *p, uintptr_t size) {
+    return fwrite(p, 1, size, f_) == size;
+  }
+  Status write(const char *msg) {
+    return write(msg, strlen(msg));
+  }
+  // TODO(pts): Don't inline write_dec.
+  // TODO(pts): Add int32_t.
+  Status write_dec(int64_t v) {
+    char buf[21], *p = buf, *q, c;
+    if (v < 0) {
+      *p++ = '-';
+      v = -v;
+    }
+    q = p;
+    do { *q++ = v % 10 + '0'; v /= 10; } while (v > 0);
+    *q-- = '\0';
+    while (p < q) {  // Reverse.
+      c = *p;
+      *p++ = *q;
+      *q-- = c;
+    }
+    return write(buf);
+  }
+  Status write_dec(uint64_t v) {
+    char buf[20], *p = buf, *q, c;
+    q = p;
+    do { *q++ = v % 10 + '0'; v /= 10; } while (v > 0);
+    *q-- = '\0';
+    while (p < q) {  // Reverse.
+      c = *p;
+      *p++ = *q;
+      *q-- = c;
+    }
+    return write(buf);
+  }
+  // TODO(pts): Implement write_hex etc.
+
+  // Most convenience functions are in `>>'.
+ private:
+  FILE *f_;  // Owned externally.
+};
+
+FileObj sin(stdin);
+// sout << "Hello\n";
+FileObj sout(stdout);
+FileObj serr(stderr);
+// io << stdout << "Hello!\n";
+static inline FileObj operator<<(Io, FILE *f) { return f; }
+static inline FileObj &operator<<(Io, FileObj &fo) { return fo; }
+static inline FileObj &operator<<(FileObj &fo, const char *msg) {
+  fo.write(msg);
+  return fo;
+}
+static inline FileObj &operator<<(FileObj &fo, const std::string &str) {
+  fo.write(str.data(), str.size());
+  return fo;
+}
+static inline FileObj &operator<<(FileObj &fo, int8_t v) {
+  fo.write_dec((int64_t)v);
+  return fo;
+}
+static inline FileObj &operator<<(FileObj &fo, int16_t v) {
+  fo.write_dec((int64_t)v);
+  return fo;
+}
+static inline FileObj &operator<<(FileObj &fo, int32_t v) {
+  fo.write_dec((int64_t)v);
+  return fo;
+}
+static inline FileObj &operator<<(FileObj &fo, int64_t v) {
+  fo.write_dec(v);
+  return fo;
+}
+static inline FileObj &operator<<(FileObj &fo, uint8_t v) {
+  fo.write_dec((uint64_t)v);
+  return fo;
+}
+static inline FileObj &operator<<(FileObj &fo, uint16_t v) {
+  fo.write_dec((uint64_t)v);
+  return fo;
+}
+static inline FileObj &operator<<(FileObj &fo, uint32_t v) {
+  fo.write_dec((uint64_t)v);
+  return fo;
+}
+static inline FileObj &operator<<(FileObj &fo, uint64_t v) {
+  fo.write_dec(v);
+  return fo;
+}
+static inline FileObj &operator<<(FileObj &fo, float v) {
+  char buf[24];
+  fmt_float(v, buf);
+  fo.write(buf);
+  return fo;
+}
+static inline FileObj &operator<<(FileObj &fo, double v) {
+  char buf[24];
+  fmt_double(v, buf);
+  fo.write(buf);
+  return fo;
+}
+
+class Flush {};
+extern Flush flush;
+
+static inline FileObj &operator<<(FileObj &fo, Flush) {
+  fo.flush();
+  return fo;
+}
+
+// TODO(pts): Add operator<< for StringOutObj.
+
+// ---
 
 // TODO(pts): Dumping: void operator~(const std::string &s) {}
 
 int main(int argc, char **argv) {
-  (void)argc; (void)argv;
+  (void)argc; (void)argv;  // TODO(pts): Do we need this? int main() { ... }
+  sout << "Hello, " << -42 << "," << 123e200 << "," << 1.23f << "!\n" << flush;
+
 #if 0
   int8_t i8 = read_dec(stdin);
   printf("i8=(%d)\n", i8);
