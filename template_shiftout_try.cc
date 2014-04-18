e6dad7db085c3b7e72ad290e35abff72aa8d1ec6
Peter Szabo <pts@fazekas.hu>
2014-04-18 16:35:57 +0200

    updated template_shiftout_try.cc

--- template_shiftout_try.cc
+++ template_shiftout_try.cc
@@ -39,7 +39,7 @@ class FileObj {
   FILE *f_;  // Owned externally.
 };
 
-#if 0
+#if 0  // TODO(pts): Remove.
 class StringWritable {
  public:
   // This is needed for convenience in my_string << dump(42).
@@ -81,6 +81,14 @@ class D {
 };
 
 template<class V>class TFormatter {};
+
+template<>class TFormatter<bool> {
+ public:
+  enum max_type { max_buf_size = 5 };  // Including the trailing \0.
+  static void format(bool v, char *buf) {
+    strcpy(buf, v ? "true" : "false");
+  }
+};
 
 template<>class TFormatter<int> {
  public:
@@ -130,6 +138,7 @@ template<>class TFormatter<const C&> {
 
 template<class T>class TWritable {};
 
+#if 0  // TODO(pts): Remove.
 template<>struct TWritable<std::string> {
   typedef void *tag_type;
   static inline void write(const char *data, uintptr_t size,
@@ -142,6 +151,7 @@ template<>struct TWritable<std::string> {
     const_cast<std::string&>(wr).append(msg);
   }
 };
+#endif
 
 template<>struct TWritable<FileObj> {
   typedef void *tag_type;
@@ -160,13 +170,21 @@ template<class First, class Second, class Third>struct TypeTriplet {
   typedef Third third_type;
 };
 
-// The return type is just `const W&', but it's formulated in a way to prevent
-// the template from matching if either TWritable<W> or TFormatter<V> is
-// not specialized. This is a tricky use of
+template<class First, class Second>struct TypePair {
+  typedef First first_type;
+  typedef Second second_type;
+};
+
+// --- Generic writing to TWritable<W>.
+//
+// For the functions below, the return type is just `const W&', but it's
+// formulated in a way to prevent the template from matching if either
+// TWritable<W> or TFormatter<V> is not specialized. This is a tricky use of
 // http://en.wikipedia.org/wiki/SFINAE .
+
 template<class W, class V>static inline
 typename TypeTriplet<const W&, typename TWritable<W>::tag_type,
-                     typename TFormatter<const V&>::max_type >::first_type
+                     typename TFormatter<const V&>::max_type>::first_type
 operator<<(const W &wr, const V &v) {
   char buf[TFormatter<const V&>::max_buf_size];
   TFormatter<const V&>::format(v, buf);
@@ -176,7 +194,7 @@ operator<<(const W &wr, const V &v) {
 
 template<class W, class V>static inline
 typename TypeTriplet<const W&, typename TWritable<W>::tag_type,
-                     typename TFormatter<V>::max_type >::first_type
+                     typename TFormatter<V>::max_type>::first_type
 operator<<(const W &wr, V v) {
   char buf[TFormatter<V>::max_buf_size];
   TFormatter<V>::format(v, buf);
@@ -186,7 +204,7 @@ operator<<(const W &wr, V v) {
 
 template<class W, class V>static inline
 typename TypeTriplet<const W&, typename TWritable<W>::tag_type,
-                     typename TFormatter<const V&>::piece_type >::first_type
+                     typename TFormatter<const V&>::piece_type>::first_type
 operator<<(const W &wr, const V &v) {
   TFormatter<const V&> fmt(v);
   TWritable<W>::write(fmt.data(), fmt.size(), wr);
@@ -195,7 +213,7 @@ operator<<(const W &wr, const V &v) {
 
 template<class W, class V>static inline
 typename TypeTriplet<const W&, typename TWritable<W>::tag_type,
-                     typename TFormatter<V>::piece_type >::first_type
+                     typename TFormatter<V>::piece_type>::first_type
 operator<<(const W &wr, V v) {
   TFormatter<V> fmt(v);
   TWritable<W>::write(fmt.data(), fmt.size(), wr);
@@ -204,40 +222,107 @@ operator<<(const W &wr, V v) {
 
 template<class W, class V>static inline
 typename TypeTriplet<const W&, typename TWritable<W>::tag_type,
-                     typename TFormatter<const V&>::append_type >::first_type
+                     typename TFormatter<const V&>::append_type>::first_type
 operator<<(const W &wr, const V &v) {
   std::string str;
   TFormatter<const V&>::format_append(v, &str);
-  // TODO(pts): Specialize for W=std::string.
   TWritable<W>::write(str.data(), str.size(), wr);
   return wr;
 }
 
 template<class W, class V>static inline
 typename TypeTriplet<const W&, typename TWritable<W>::tag_type,
-                     typename TFormatter<V>::append_type >::first_type
+                     typename TFormatter<V>::append_type>::first_type
 operator<<(const W &wr, V v) {
   std::string str;
   TFormatter<V>::format_append(v, &str);
-  // TODO(pts): Specialize for W=std::string.
   TWritable<W>::write(str.data(), str.size(), wr);
   return wr;
 }
 
 template<class W, class V>static inline
 typename TypeTriplet<const W&, typename TWritable<W>::tag_type,
-                     typename V::format_append_type >::first_type
+                     typename V::format_append_type>::first_type
 operator<<(const W &wr, const V &v) {
   std::string str;
   v.format_append(&str);
-  // TODO(pts): Specialize for W=std::string.
   TWritable<W>::write(str.data(), str.size(), wr);
   return wr;
 }
 
+// --- Writing to std::string.
+//
+// We could implement a TWritable<std::string>, but some of the
+// implementations below are a bit more optimized. They also don't need
+// const_cast.
+
+template<class V>static inline
+typename TypePair<std::string&,
+                  typename TFormatter<const V&>::max_type>::first_Type
+operator<<(std::string &wstr, const V &v) {
+  char buf[TFormatter<const V&>::max_buf_size];
+  TFormatter<const V&>::format(v, buf);
+  wstr.append(buf);
+  return wstr;
+}
+
+template<class V>static inline
+typename TypePair<std::string&,
+                  typename TFormatter<V>::max_type>::first_type
+operator<<(std::string &wstr, V v) {
+  char buf[TFormatter<V>::max_buf_size];
+  TFormatter<V>::format(v, buf);
+  wstr.append(buf);
+  return wstr;
+}
+
+template<class V>static inline
+typename TypePair<std::string&,
+                  typename TFormatter<const V&>::piece_type>::first_type
+operator<<(std::string &wstr, const V &v) {
+  TFormatter<const V&> fmt(v);
+  wstr.append(fmt.data(), fmt.size());
+  return wstr;
+}
+
+template<class V>static inline
+typename TypePair<std::string&,
+                  typename TFormatter<V>::piece_type>::first_type
+operator<<(std::string &wstr, V v) {
+  TFormatter<V> fmt(v);
+  wstr.append(fmt.data(), fmt.size());
+  return wstr;
+}
+
+template<class V>static inline
+typename TypePair<std::string&,
+                  typename TFormatter<const V&>::append_type>::first_type
+operator<<(std::string &wstr, const V &v) {
+  TFormatter<const V&>::format_append(v, &wstr);  // Optimized implementation.
+  return wstr;
+}
+
+template<class V>static inline
+typename TypePair<std::string&,
+                  typename TFormatter<V>::append_type>::first_type
+operator<<(std::string &wstr, V v) {
+  TFormatter<V>::format_append(v, &wstr);  // Optimized implementation.
+  return wstr;
+}
+
+template<class V>static inline
+typename TypePair<std::string&,
+                  typename V::format_append_type>::first_type
+operator<<(std::string &wstr, const V &v) {
+  v.format_append(&wstr);  // Optimized implementation.
+  return wstr;
+}
+
+// ---
+
 int main() {
   // To make FileObj(stdout) or `fo(stdout)' work, we need `const W&'.
-  fprintf((FileObj(stdout) << 42 << -5).f(), ".\n");
+  fprintf((FileObj(stdout) << 42 << -5 << C()).f(), ".\n");
   // SUXX: No way to make it work like this.
   // This doesn't work without the explicit operator<<(std:: string &,...).
   // printf("%s!\n", (std::string() << 42 << -5).c_str()); // (S).
@@ -253,7 +338,8 @@ int main() {
   s << D();
   printf("</C>\n");
   s << "Foo" << std::string("Bar");
+  FileObj(stdout) << true;
+  // FileObj(stdout) << 4.5;  // Doesn't compile, TFormatter<double> not defined.
   printf("S=(%s)\n", s.c_str());
-  // FileObj(stdout) << true;  // Doesn't compile, TFormatter<bool> not defined.
   return 0;
 }
