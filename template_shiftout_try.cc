e1f3343a4d967a60cbe070207e5322b32013d2fb
Peter Szabo <pts@fazekas.hu>
2014-04-18 16:34:49 +0200

    added template_shiftout_try.cc

---
+++ template_shiftout_try.cc
@@ -0,0 +1,79 @@
+#include <stdint.h>
+#include <stdio.h>
+#include <string.h>
+
+#include <string>
+
+#include "r_status.h"
+
+class FileObj {
+ public:
+  FileObj(FILE *f): f_(assume_notnull(f)) {}
+  FILE *f() const { return f_; }
+  operator FILE*() { return f_; }
+  Status flush() const {
+    return fflush(f_) == 0;
+  }
+  // TODO(pts): move to write(FILE *f, ...)?
+  Status write(const void *p, uintptr_t size) const {
+    // TODO(pts): Add proper error message. (Also elsewhere.)
+    return fwrite(p, 1, size, f_) == size;
+  }
+  Status write(const char *msg) const {
+    return write(msg, strlen(msg));
+  }
+  // Implements method from Writable.
+  // TODO(pts): Move this to the .cc file.
+  //virtual void vi_write(const void *p, uintptr_t size) {
+  //  Status(fwrite(p, 1, size, f_) == size);
+  //}
+  // Implements method from Writable.
+  // TODO(pts): Move this to the .cc file.
+  //virtual void vi_putc(char c) {
+  //  Status(putc(c, f_) > 0);
+  //}
+
+  // Most convenience functions are in `>>'.
+ private:
+  FILE *f_;  // Owned externally.
+};
+
+class StringWritable {
+ public:
+  // This is needed for convenience in my_string << dump(42).
+  StringWritable(std::string &str): str_(&str) {}
+  StringWritable(std::string *str): str_(assume_notnull(str)) {}
+  std::string *str() const { return str_; }
+  const char *c_str() const { return str_->c_str(); }
+  //virtual void vi_write(const void *p, uintptr_t size) {
+  //  str_->append(static_cast<const char *>(p), size);
+  //}
+  //virtual void vi_putc(char c) {
+  //  str_->push_back(c);
+  //}
+ private:
+  std::string *str_;
+};
+
+//template<class T>static inline typename Formatter<T>::return_type
+const StringWritable &operator<<(const StringWritable &wr, int) {
+  //Formatter<T>::format(const_cast<StringWritable*>(&wr), t);  // TODO(pts): Fix const_cast.
+  return wr;
+}
+
+const FileObj &operator<<(const FileObj &wr, int) {
+  //Formatter<T>::format(const_cast<StringWritable*>(&wr), t);  // TODO(pts): Fix const_cast.
+  return wr;
+}
+
+int main() {
+  fprintf((FileObj(stdout) << 42 << -5).f(), ".\n");
+  // SUXX: This doesn't work, no automatic conversion. Why?
+  // TODO(pts): Add manual conversion.
+  // printf("%s;\n", (std::string() << 42 << -5).c_str());
+  std::string s;
+  // This wouldn't work if operator<< accepted `StringWritable&' instead of
+  // `const StringWritable&'.
+  printf("%s;\n", (s << 42 << -5).c_str());
+  return 0;
+}
