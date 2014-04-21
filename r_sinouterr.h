76d178d5e0e7c2fd90f83c2bdd2e7fad67451658
Peter Szabo <pts@fazekas.hu>
2014-04-21 18:05:24 +0200

    fixed link error with g++ -O0

--- r_sinouterr.h
+++ r_sinouterr.h
@@ -8,13 +8,26 @@
 #include <stdio.h>
 
 struct Sin { inline operator FILE*() const { return stdin; } };
+#ifdef __GNUC__
+static Sin sin __attribute__((used));
+#else
+// This produces a link error with `g++ -O0'.
 extern Sin sin;  // No need to actually define it anywhere.
+#endif
 
 struct Sout { inline operator FILE*() const { return stdout; } };
+#ifdef __GNUC__
+static Sout sout __attribute__((used));
+#else
 extern Sout sout;  // No need to actually define it anywhere.
+#endif
 
 struct Serr { inline operator FILE*() const { return stderr; } };
+#ifdef __GNUC__
+static Serr serr __attribute__((used));
+#else
 extern Serr serr;  // No need to actually define it anywhere.
+#endif
 
 template<class T>class TStdStream {};
 template<>struct TStdStream<Sin>  { typedef void *tag_type; };
